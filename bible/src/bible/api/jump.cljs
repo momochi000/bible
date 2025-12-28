f3e01450e56ce1dff88a8a549d5d6b460c4411c8
Zachery Chin <zacherychin@Zacherys-MacBook-Air.local>
2025-12-28 10:39:08 -0800

    when jumping to a new chapter, set verse to 1

--- bible/src/bible/api/jump.cljs
+++ bible/src/bible/api/jump.cljs
@@ -14,13 +14,13 @@
   [app-state new-chapter] ;; new-chapter should be a number but we'll cast it to be safe
   (let [new-chapter-int (js/parseInt new-chapter)
     curr-chapter (:current-chapter @app-state) ]
+    (swap! app-state assoc :current-verse 1)
     (if (not= curr-chapter new-chapter-int)
        (swap! app-state assoc :current-chapter new-chapter-int))))
 
 (defn change-verse!
   "Change the currently active/selected verse of the bible"
-  [app-state new-book]
+  [app-state new-verse]
   (let [curr-verse (:current-verse @app-state)]
-    ;;(swap! app-state assoc :current-verse 1)
     (if (not= curr-verse new-verse)
        (swap! app-state assoc :current-verse new-verse))))
